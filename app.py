02024c77063011b05f6c2c946c052d1df50bf0f9
firststepto100 <firststepto100@gmail.com>
2022-12-14 20:59:31 +0100

    2

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ def add(a , b):
     answer = a + b
     print(str(a) + "+" + str(b) + "=" + str(answer))
 
-def sub(a,b):
+def sub(a , b):
     answer = a - b
     print(str(a) + "-" + str(b) + "=" + str(answer))
 
-def mul(a,b):
-    answer = a+b
-    print(str(a) + "*" str(b) + "=" + str(answer))
+def mul(a , b):
+    answer = a * b
+    print(str(a) + "*" + str(b) + "=" + str(answer))
 
-def div(a,b):
-    answer = a+b
+def div(a , b):
+    answer = a / b
     print(str(a) + "/" + str(b) + "=" + str(answer))            
 
 
@@ -26,3 +26,27 @@ if choice == "a" or "A":
     a = int(input("Add first number: "))
     b = int(input("Add second number: "))
     add(a , b)
+
+elif choice == "b" or "B":
+    print("Subtraction")
+    a = int(input("Add first number: "))
+    b = int(input("Add second number: "))
+    sub(a , b)
+
+elif choice == "c" or "C":
+    print("Multiplication")
+    a = int(input("Add first number: "))
+    b = int(input("Add second number: "))
+    mul(a , b)   
+
+elif choice == "d" or "D":
+    print("Division")
+    a = int(input("Add first number: "))
+    b = int(input("Add second number: "))
+    div(a , b)    
+
+elif choice == "e" or "E":
+    print("program ended")
+    quit() 
+
+    
